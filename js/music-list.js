2eaea4d8a515e57fc1a5977446304363206cc5df
Min Khant Thar <124900463+myminethealmighty@users.noreply.github.com>
2023-08-24 18:58:44 +0630

    initial commit

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+  {
+    name: "Almost Easy",
+    artist: "Avenged Sevenfold",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Gossip",
+    artist: "Maneskin",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Coming Undone",
+    artist: "KoRn",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bat Country",
+    artist: "Avenged Sevenfold",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Youth Gone Wild",
+    artist: "Skid Row",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "The Vengeful One",
+    artist: "Disturbed",
+    img: "music-6",
+    src: "music-6"
+  },
+  
+];
